04beff2387d2bdef11d977361e710be7616feae9
Sylviachinny <chinyeresylvia@gmail.com>
2023-06-16 11:46:42 -0700

    using put

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+/**
+ * main - It prints out output using put
+ * Return: 0
+ */
+int main(void)
+{
+puts("\"Programming is like building a multilingual puzzle");
+return (0);
+}
